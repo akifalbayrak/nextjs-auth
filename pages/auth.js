2ea689fb1cb8df0b38b245c826ddb1031e92ca2f
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-24 12:00:55 +0300

    chore: Add next/router for navigation in auth form component

--- pages/auth.js
+++ pages/auth.js
@@ -1,7 +1,21 @@
-import AuthForm from '../components/auth/auth-form';
+import { useEffect } from "react";
+import AuthForm from "../components/auth/auth-form";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function AuthPage() {
-  return <AuthForm />;
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+    useEffect(() => {
+        getSession().then((session) => {
+            if (session) {
+                router.replace("/");
+            } else {
+                setIsLoading(false);
+            }
+        });
+    }, [router]);
+    return <AuthForm />;
 }
 
 export default AuthPage;
