aa729e78ccb347a80661a4d7d297e9fe1d271cfa
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-24 11:50:01 +0300

    refactor: Improve user profile authentication flow and loading state

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,15 +1,31 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession, getSession } from "next-auth/react";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+import { useEffect, useState } from "react";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+    const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+    useEffect(() => {
+        getSession().then((session) => {
+            setIsLoading(false);
+            if (!session) {
+                window.location.href = "/auth";
+            } else {
+                setIsLoading(false);
+            }
+        });
+    });
+    if (isLoading) {
+        return <p className={classes.profile}>Loading...</p>;
+    }
+
+    return (
+        <section className={classes.profile}>
+            <h1>Your User Profile</h1>
+            <ProfileForm />
+        </section>
+    );
 }
 
 export default UserProfile;
